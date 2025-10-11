134e9145255e6cb3417729cd440d56e2791ecc56
Salsabeel Ibrahim <Salsabeeladam281@gmail.com>
2025-10-11 09:19:41 +0300

    task4/r34-12: fixed some errors in project

---
+++ lib/task4/r34_12/lib/features/posts/data/models/post_model.dart
@@ -0,0 +1,44 @@
+import 'package:r34_12/features/posts/domain/entities/post.dart';
+
+class PostModel extends Post {
+  const PostModel({
+    required super.id,
+    required super.title,
+    required super.body,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'].toString(),
+      title: (json['title'] ?? '') as String,
+      body: (json['body'] ?? '') as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  Map<String, dynamic> toJsonForCreate() {
+    return {
+      'title': title,
+      'body': body,
+    };
+  }
+
+  PostModel copyWith({
+    String? id,
+    String? title,
+    String? content,
+  }) {
+    return PostModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: content ?? this.body,
+    );
+  }
+}
